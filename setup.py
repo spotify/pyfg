23056fcbf2c0738b532f5da21f6733b7a7a82a5c
David Barroso <dbarroso@spotify.com>
2015-03-22 15:31:05 +0100

    Incremented version

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
+import uuid
+
 from setuptools import setup, find_packages
 from pip.req import parse_requirements
 
 # parse_requirements() returns generator of pip.req.InstallRequirement objects
-install_reqs = parse_requirements('requirements.txt')
+install_reqs = parse_requirements('requirements.txt', session=uuid.uuid1())
 
 # reqs is a list of requirement
 # e.g. ['django==1.5.1', 'mezzanine==1.4.6']
