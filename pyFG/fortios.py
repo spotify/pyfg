9e630e7f1fc5c73d30f9922eaa95fa171c7c3032
Sergio Rua <sergio.rua@sky.uk>
2015-09-14 12:33:57 +0100

    tiny bug: it would break without ~/.ssh/config

--- pyFG/fortios.py
+++ pyFG/fortios.py
@@ -62,13 +62,6 @@ class FortiOS(object):
         self.ssh = paramiko.SSHClient()
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-        if os.path.exists(os.path.expanduser("~/.ssh/config")):
-          ssh_config = paramiko.SSHConfig()
-          user_config_file = os.path.expanduser("~/.ssh/config")
-          with open(user_config_file) as f:
-            ssh_config.parse(f)
-          f.close()
-
         cfg = {
           'hostname': self.hostname, 
           'timeout': 10,
@@ -76,11 +69,18 @@ class FortiOS(object):
           'password': self.password,
           'key_filename': self.keyfile
         }
-        host_conf = ssh_config.lookup(self.hostname)
-        if host_conf:
-          if 'proxycommand' in host_conf:
-            cfg['sock'] = paramiko.ProxyCommand(host_conf['proxycommand'])
 
+        if os.path.exists(os.path.expanduser("~/.ssh/config")):
+          ssh_config = paramiko.SSHConfig()
+          user_config_file = os.path.expanduser("~/.ssh/config")
+          with open(user_config_file) as f:
+            ssh_config.parse(f)
+          f.close()
+
+          host_conf = ssh_config.lookup(self.hostname)
+          if host_conf:
+            if 'proxycommand' in host_conf:
+              cfg['sock'] = paramiko.ProxyCommand(host_conf['proxycommand'])
 
         self.ssh.connect(**cfg)
 
